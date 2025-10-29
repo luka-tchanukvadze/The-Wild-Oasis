dd449d1db0382cf4b27296f1a83a72dc537d051e
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-10-29 18:40:30 +0400

    Fetching Data in a Page

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,4 +1,19 @@
-function Page() {
-  return <div>Cabins page</div>;
+async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  console.log(data);
+
+  return (
+    <div>
+      <h1>Cabins page</h1>
+
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 export default Page;
