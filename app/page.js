1a373c11762b1619de624f7a12da186671a0bd54
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-10-29 14:14:06 +0400

    Creating a Layout

--- app/page.js
+++ app/page.js
@@ -4,7 +4,6 @@ import Navigation from "./components/Navigation";
 export default function Page() {
   return (
     <>
-      <Navigation />
       <div>hey the wild oasis</div>;
       <Link href={"/cabins"}>Explore luxury cabins</Link>
     </>
