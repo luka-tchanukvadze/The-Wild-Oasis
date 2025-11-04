f9548b6305517e77ddd946c47335776105fc055c
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-11-04 14:55:54 +0400

    Improving the Navigation and Root Layout

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,4 @@
-import Logo from "./_components/Logo";
-import Navigation from "./_components/Navigation";
+import Header from "./_components/Header";
 
 import "@/app/_styles/globals.css";
 
@@ -24,14 +23,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
       >
-        <header>
-          <Logo />
-        </header>
-        <Navigation />
-        {children}
-        <footer>Copyright by The Wild Oasis</footer>
+        <Header />
+
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
