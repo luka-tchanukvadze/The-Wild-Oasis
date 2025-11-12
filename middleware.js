f6fef3d3d779598b70bec3b94dadfd4fbb00f3f6
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-11-12 15:25:17 +0400

    Protecting Routes With NextAuth Middleware

---
+++ middleware.js
@@ -0,0 +1,18 @@
+import { auth } from "./app/_lib/auth";
+// import { NextResponse } from "next/server";
+
+// export function middleware(request) {
+//   console.log(request);
+
+//   return NextResponse.redirect(new URL("/about", request.url));
+// }
+
+// export const config = {
+//   matcher: ["/account"],
+// };
+
+export const middleware = auth;
+
+export const config = {
+  matcher: ["/account"],
+};
