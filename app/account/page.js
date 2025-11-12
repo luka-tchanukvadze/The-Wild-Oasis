2571600f56ac701e4ebb14856c4a80f2a6458ef8
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-11-12 15:39:46 +0400

    Building a Custom Sign In Page

--- app/account/page.js
+++ app/account/page.js
@@ -1,11 +1,16 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "Guest area",
 };
 
-function page() {
+async function page() {
+  const session = await auth();
+  const firstName = session.user.name.split(" ").at(0);
+
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-      Welcome, Chanu
+      Welcome, {firstName}
     </h2>
   );
 }
