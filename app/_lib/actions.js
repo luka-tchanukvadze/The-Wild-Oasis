2c35a5ca14634dbf38cd2e5ae590bbcee10f6da3
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-11-18 15:14:15 +0400

    Deleting a Reservation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -30,6 +30,12 @@ export async function deleteReservation(bookingId) {
   const session = await auth();
   if (!session) throw new Error("You must be logged in");
 
+  const guestBookings = await getBookings(SquareAsteriskIcon.user.guestId);
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingIds.includes(bookingId))
+    throw new Error("You are not allowed to delte this booking");
+
   const { error } = await supabase
     .from("bookings")
     .delete()
