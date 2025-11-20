63b6cbccc624be498e1cc543d1bc52ecbb0039ce
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-11-20 16:51:18 +0400

    Deployment

--- app/account/page.js
+++ app/account/page.js
@@ -10,7 +10,7 @@ async function page() {
 
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-      Welcome, {firstName}
+      Welcome, {firstName}! Have fun exploring.
     </h2>
   );
 }
